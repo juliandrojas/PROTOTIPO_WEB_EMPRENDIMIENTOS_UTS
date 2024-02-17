e67b4229fef798bbedfe4db1cb4b32e8ccaebfc9
Julian Rojas <juliandra140201@gmail.com>
2024-02-16 23:08:39 -0500

    Add("Loader data and conditionals")

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -4,14 +4,17 @@ import { Link } from 'react-router-dom';
 
 function Index() {
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await axios.get('http://localhost:3000/api/emprendimientos');
                 setData(response.data);
+                setLoading(false);
             } catch (error) {
                 console.error('Error accediendo a la data: ' + error);
+                setLoading(false);
             }
         };
         fetchData();
@@ -25,22 +28,38 @@ function Index() {
                 <Link to={'/register'} className='btn btn-primary w-100 mt-3'>
                     ¿Aún no has registrado tu emprendimiento? ¡Haz clic aquí para empezar!
                 </Link>
-                <div className="mt-3"> {/* Agrega un margen superior */}
-                    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-                        {data.map(item => (
-                            <div className="col" key={item.id}>
-                                <div className="card h-100">
-                                    <div className="card-body">
-                                        <h5 className="card-title">{item.nombre}</h5>
-                                        <p className="card-text">Número: {item.numero}</p>
-                                        <p className="card-text">Creador: {item.creador}</p>
-                                        <p className="card-text">Tipo: {item.tipo}</p>
+                {loading ? (
+                    <div className="text-center mt-3">
+                        <p>Obteniendo datos...</p>
+                        {/* Aquí puedes agregar un spinner de carga si lo deseas */}
+                    </div>
+                ) : data.length ? (
+                    <div className="mt-3">
+                        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                            {data.map(item => (
+                                <div className="col" key={item.id}>
+                                    <div className="card h-100">
+                                        <div className="card-body">
+                                            <h5 className="card-title">{item.nombre}</h5>
+                                            <p className="card-text">Número: {item.numero}</p>
+                                            <p className="card-text">Creador: {item.creador}</p>
+                                            <p className="card-text">Tipo: {item.tipo}</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
-                </div>
+                ) : (
+                    <div className="card mt-5 h-100">
+                        <div className="card-body">
+                            <h5 className="card-title text-center">No hay datos disponibles</h5>
+                        </div>
+                    </div>
+                    // <div className="text-center mt-3">
+                    //     <p>No hay datos disponibles.</p>
+                    // </div>
+                )}
             </div>
         </>
     )
